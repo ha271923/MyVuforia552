daba431ca2ea75f12b4babf64c07db7e8d9ee5c1
ha271923 <ha271923@gmail.com>
2017-12-01 17:57:05 +0800

    Vuforia: support VirtualButton press/release behavior

---
+++ Assets/_demo/vBtnScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vBtnScript : MonoBehaviour, IVirtualButtonEventHandler {
+	public GameObject virtualButtonObject;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log("vBtnScript:: Start");
+		virtualButtonObject = GameObject.Find("MyVirtualButton1");
+		virtualButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log("Update");
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log("OnButtonPressed +++");
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+	{
+		Debug.Log("OnButtonReleased ---");
+	}
+
+
+}
